36f402ef4665c7db1b79954b77b5c2cac8859513
Spencer Dyvig <sd31117@hotmail.com>
2020-07-01 23:07:56 -0500

    Update backupMaintainer.py

--- scripts/backupMaintainer.py
+++ scripts/backupMaintainer.py
@@ -1,6 +1,9 @@
+#This script maintains a folder on the machine and only keeps items from the last 30 days (mins, sec, etc.)
+#depending on how many backups you make and how often the script is ran.
+
 import os
 
-path = '../backup/'
+path = 'Enter backup path here..'
 os.chdir(path)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
